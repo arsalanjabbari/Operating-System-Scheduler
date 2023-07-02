d63f96b1ee000c3048dd29726bb7164c43b3e80c
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-03 03:21:37 +0330

    Make Queue.py ready!

---
+++ Queue.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+        self.front =
+        self.size =
+        self.last =
+        self.capacity =
+        # array of processes - write this code
+
+    def create_queue(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+
