eb33913f21c6528cc9517f9882a99f0837a0c3e8
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-03 03:14:27 +0330

    Make Process.py ready!

---
+++ Process.py
@@ -0,0 +1,17 @@
+class Process:
+    def __init__(self):
+        self.process_id =
+        self.arrival_time =
+        self.CPU_burst_1_time =
+        self.CPU_burst_2_time =
+        self.IO_burst_time =
+        self.response_time =
+        self.turn_around_time =
+        self.waiting_time =
+        self.termination_time =
+
+    def create_process(self):
+        pass
+
+    def print_process(self):
+        pass
