628a90fedf5681aed7badd25f59daf6e4f97cd5d
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-03 19:17:20 +0330

    Implement Process.py.

--- Process.py
+++ Process.py
@@ -1,17 +1,28 @@
+# Process class in scheduling unit of operating system (OS)
 class Process:
-    def __init__(self):
-        self.process_id =
-        self.arrival_time =
-        self.CPU_burst_1_time =
-        self.CPU_burst_2_time =
-        self.IO_burst_time =
-        self.response_time =
-        self.turn_around_time =
-        self.waiting_time =
-        self.termination_time =
+    def __init__(self, process_id, arrival_time, CPU_burst_time_1, IO_burst_time, CPU_burst_time_2):
+        self.process_id = process_id
+        self.arrival_time = arrival_time
+        self.CPU_burst_time_1 = CPU_burst_time_1
+        self.IO_burst_time = IO_burst_time
+        self.CPU_burst_time_2 = CPU_burst_time_2
+        self.response_time = -1
+        self.turn_around_time = -1
+        self.waiting_time = -1
+        self.termination_time = -1
 
-    def create_process(self):
-        pass
+    def create_new_process(process_id, arrival_time, CPU_burst_time_1, IO_burst_time, CPU_burst_time_2):
+        process = Process(process_id, arrival_time, CPU_burst_time_1, IO_burst_time, CPU_burst_time_2)
+        return process
 
-    def print_process(self):
-        pass
+    def copy_process(process):
+        new_process = Process(process.process_id, process.arrival_time, process.CPU_burst_time_1, process.IO_burst_time,
+                              process.CPU_burst_time_2)
+        new_process.response_time = process.response_time
+        new_process.turn_around_time = process.turn_around_time
+        new_process.waiting_time = process.waiting_time
+        new_process.termination_time = process.termination_time
+        return new_process
+
+    def print_process(process):
+        print(f"Process ID: {process.process_id} | Arrival Time: {process.arrival_time} | CPU Burst Time 1: {process.CPU_burst_time_1} | IO Burst Time: {process.IO_burst_time} | CPU Burst Time 2: {process.CPU_burst_time_2} | Response Time: {process.response_time} | Turn Around Time: {process.turn_around_time} | Waiting Time: {process.waiting_time} | Termination Time: {process.termination_time}")
