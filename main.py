3616df8cdee58581e7cbba6eddd086b0f9580a70
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-04 22:00:37 +0330

    RR With Test, Alright!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     job_queue = read_processes_from_CSV_file("csv.csv")
     sort_queue_at(job_queue)
     FCFS_scheduling_algorithm(copy_queue(job_queue))
-    # RR_scheduling_algorithm(job_queue.copy())
+    RR_scheduling_algorithm(copy_queue(job_queue))
     # SJF_scheduling_algorithm(job_queue.copy())
     # SRTF_scheduling_algorithm(job_queue.copy())
     # MLFQ_scheduling_algorithm(job_queue.copy())
