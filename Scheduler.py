9d227762cabaa0f073d35d6c8a7047e85f31b609
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-05 22:49:24 +0330

    Scheduler.py Update!

--- Scheduler.py
+++ Scheduler.py
@@ -3,8 +3,8 @@ from Process_Analyze import analyze_processes
 from Queue import *
 from Test_Section import create_directory, create_file
 
-def FCFS_scheduling_algorithm(job_queue):
 
+def FCFS_scheduling_algorithm(job_queue):
     create_directory("./output/FCFS")
     algorithm_procedure_output_file = create_file("./output/FCFS/", "FCFS - Algorithm Procedure", ".log")
     algorithm_analysis_output_file = create_file("./output/FCFS/", "FCFS - Algorithm Analysis", ".log")
@@ -56,7 +56,7 @@ def FCFS_scheduling_algorithm(job_queue):
                         running_process.termination_time = current_time + 1
                         running_process.turn_around_time = running_process.termination_time - running_process.arrival_time
                         running_process.waiting_time = running_process.turn_around_time - (
-                                    running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
+                                running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
                         running_process = None
                         algorithm_procedure_output_file.write(
                             f"\tProcess-{rear(terminated_queue).process_id} Was Terminated (Moved From Running-State to Terminated-Queue).\n")
@@ -72,7 +72,6 @@ def FCFS_scheduling_algorithm(job_queue):
 
         current_time += 1
 
-
     algorithm_procedure_output_file.close()
 
     analyze_algorithm(job_queue_copy, terminated_queue, algorithm_analysis_output_file)
@@ -81,8 +80,8 @@ def FCFS_scheduling_algorithm(job_queue):
     algorithm_analysis_output_file.close()
     processes_analysis_output_file.close()
 
-def RR_scheduling_algorithm(job_queue):
 
+def RR_scheduling_algorithm(job_queue):
     create_directory("./output/RR")
     algorithm_procedure_output_file = create_file("./output/RR/", "RR - Algorithm Procedure", ".log")
     algorithm_analysis_output_file = create_file("./output/RR/", "RR - Algorithm Analysis", ".log")
@@ -97,8 +96,8 @@ def RR_scheduling_algorithm(job_queue):
     current_time = 0
     algorithm_procedure_output_file.write("*** Round-Robin (RR) Scheduling Algorithm in Operating System "
                                           "***\n\n")
-    print("Enter your desired time-quantum:")
-    time_quantum = int(input())
+
+    time_quantum = int(input("Enter your desired time-quantum: "))
     temp_elapsed_time = 0
 
     while not is_full(terminated_queue):
@@ -144,7 +143,7 @@ def RR_scheduling_algorithm(job_queue):
                         running_process.termination_time = current_time + 1
                         running_process.turn_around_time = running_process.termination_time - running_process.arrival_time
                         running_process.waiting_time = running_process.turn_around_time - (
-                                    running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
+                                running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
                         running_process = None
                         algorithm_procedure_output_file.write(
                             "\tProcess-{} Was Terminated (Moved From Running-State to Terminated-Queue).\n".format(
@@ -179,15 +178,13 @@ def RR_scheduling_algorithm(job_queue):
         current_time += 1
 
     algorithm_procedure_output_file.close()
-
     analyze_algorithm(job_queue_copy, terminated_queue, algorithm_analysis_output_file)
     analyze_processes(job_queue_copy, terminated_queue, processes_analysis_output_file)
-
     algorithm_analysis_output_file.close()
     processes_analysis_output_file.close()
 
-def SPN_scheduling_algorithm(job_queue):
 
+def SPN_scheduling_algorithm(job_queue):
     create_directory("./output/SPN")
     algorithm_procedure_output_file = create_file("./output/SPN/", "SPN - Algorithm Procedure", ".log")
     algorithm_analysis_output_file = create_file("./output/SPN/", "SPN - Algorithm Analysis", ".log")
@@ -202,6 +199,7 @@ def SPN_scheduling_algorithm(job_queue):
     current_time = 0
     algorithm_procedure_output_file.write("*** Shortest-Process-Next (SPN) Scheduling Algorithm in Operating System "
                                           "***\n\n")
+
     while not is_full(terminated_queue):
         algorithm_procedure_output_file.write("Time = {}-{}:\n".format(current_time, current_time + 1))
 
@@ -242,7 +240,7 @@ def SPN_scheduling_algorithm(job_queue):
                         running_process.termination_time = current_time + 1
                         running_process.turn_around_time = running_process.termination_time - running_process.arrival_time
                         running_process.waiting_time = running_process.turn_around_time - (
-                                    running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
+                                running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
                         running_process = None
                         algorithm_procedure_output_file.write(
                             "\tProcess-{} Was Terminated (Moved From Running-State to Terminated-Queue).\n".format(
@@ -263,15 +261,13 @@ def SPN_scheduling_algorithm(job_queue):
         current_time += 1
 
     algorithm_procedure_output_file.close()
-
     analyze_algorithm(job_queue_copy, terminated_queue, algorithm_analysis_output_file)
     analyze_processes(job_queue_copy, terminated_queue, processes_analysis_output_file)
-
     algorithm_analysis_output_file.close()
     processes_analysis_output_file.close()
 
-def SRTF_scheduling_algorithm(job_queue):
 
+def SRTF_scheduling_algorithm(job_queue):
     create_directory("./output/SRTF")
     algorithm_procedure_output_file = create_file("./output/SRTF/", "SRTF - Algorithm Procedure", ".log")
     algorithm_analysis_output_file = create_file("./output/SRTF/", "SRTF - Algorithm Analysis", ".log")
@@ -284,8 +280,9 @@ def SRTF_scheduling_algorithm(job_queue):
     terminated_queue = Queue(job_queue.capacity)
 
     current_time = 0
-    algorithm_procedure_output_file.write("*** Shortest-Remaining-Time-First (SRTF) Scheduling Algorithm in Operating System "
-                                          "***\n\n")
+    algorithm_procedure_output_file.write(
+        "*** Shortest-Remaining-Time-First (SRTF) Scheduling Algorithm in Operating System "
+        "***\n\n")
 
     while not is_full(terminated_queue):
         algorithm_procedure_output_file.write("Time = {}-{}:\n".format(current_time, current_time + 1))
@@ -327,7 +324,7 @@ def SRTF_scheduling_algorithm(job_queue):
                         running_process.termination_time = current_time + 1
                         running_process.turn_around_time = running_process.termination_time - running_process.arrival_time
                         running_process.waiting_time = running_process.turn_around_time - (
-                                    running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
+                                running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
                         running_process = None
                         algorithm_procedure_output_file.write(
                             "\tProcess-{} Was Terminated (Moved From Running-State to Terminated-Queue).\n".format(
@@ -360,15 +357,13 @@ def SRTF_scheduling_algorithm(job_queue):
         current_time += 1
 
     algorithm_procedure_output_file.close()
-
     analyze_algorithm(job_queue_copy, terminated_queue, algorithm_analysis_output_file)
     analyze_processes(job_queue_copy, terminated_queue, processes_analysis_output_file)
-
     algorithm_analysis_output_file.close()
     processes_analysis_output_file.close()
 
-def MLFQ_scheduling_algorithm(job_queue):
 
+def MLFQ_scheduling_algorithm(job_queue):
     create_directory("./output/MLFQ")
     algorithm_procedure_output_file = create_file("./output/MLFQ/", "MLFQ - Algorithm Procedure", ".log")
     algorithm_analysis_output_file = create_file("./output/MLFQ/", "MLFQ - Algorithm Analysis", ".log")
@@ -381,15 +376,148 @@ def MLFQ_scheduling_algorithm(job_queue):
     terminated_queue = Queue(job_queue.capacity)
 
     current_time = 0
-    algorithm_procedure_output_file.write("*** Multi-Level-Feedback-Queue (MLFQ) Scheduling Algorithm in Operating System "
-                                          "***\n\n")
+    temp_elapsed_time = 0
+    process_previous_running_level = 1
 
+    algorithm_procedure_output_file.write(
+        "*** Multi-Level-Feedback-Queue (MLFQ) Scheduling Algorithm in Operating System "
+        "***\n\n")
 
+    number_of_queues = int(input("How many Queue you want for your scheduling: "))
+    queues_quantum = [0] * number_of_queues
+    for i in range(1, number_of_queues + 1):
+        temp_time_quantum = int(input(f"Enter quantum-time for Queue number {i}"))
+        queues_quantum[i] = temp_time_quantum
+
+    while not is_full(terminated_queue):
+        algorithm_procedure_output_file.write("Time = {}-{}:\n".format(current_time, current_time + 1))
+
+        if not is_empty(job_queue) and front(job_queue).arrival_time <= current_time:
+            enqueue(ready_queue, dequeue(job_queue))
+            algorithm_procedure_output_file.write(
+                "\tProcess-{} Moved From Job-Queue to Ready-Queue Level 1.\n".format(rear(ready_queue).process_id))
+
+        if not is_empty(ready_queue) and running_process is None:
+            running_process = dequeue(ready_queue)
+            running_process_previous_level = 1
+            running_elapsed_time = 0
+            if running_process.response_time == -1:
+                running_process.response_time = current_time
+            algorithm_procedure_output_file.write(
+                "\tProcess-{} Moved From Ready-Queue Level 1 to Running-State.\n".format(running_process.process_id))
+        elif not is_empty(ready_queue_level_2) and running_process is None:
+            running_process = dequeue(ready_queue_level_2)
+            running_process_previous_level = 2
+            running_elapsed_time = 0
+            if running_process.response_time == -1:
+                running_process.response_time = current_time
+            algorithm_procedure_output_file.write(
+                "\tProcess-{} Moved From Ready-Queue Level 2 to Running-State.\n".format(running_process.process_id))
+        elif not is_empty(ready_queue_level_3) and running_process is None:
+            running_process = dequeue(ready_queue_level_3)
+            running_process_previous_level = 3
+            running_elapsed_time = 0
+            if running_process.response_time == -1:
+                running_process.response_time = current_time
+            algorithm_procedure_output_file.write(
+                "\tProcess-{} Moved From Ready-Queue Level 3 to Running-State.\n".format(running_process.process_id))
+
+        if not is_empty(waiting_queue):
+            front(waiting_queue).IO_burst_time -= 1
+            algorithm_procedure_output_file.write(
+                "\tProcess-{}'s Waited for I/O Recourse for 1 Second (Remaining I/O Waiting Time = {}).\n".format(
+                    front(waiting_queue).process_id, front(waiting_queue).IO_burst_time))
+
+            if front(waiting_queue).IO_burst_time == 0:
+                enqueue(ready_queue, dequeue(waiting_queue))
+                algorithm_procedure_output_file.write(
+                    "\tProcess-{}'s IO's Waiting Time Was Finished and Was Moved From Waiting-Queue to Ready-Queue.\n".format(
+                        rear(ready_queue).process_id))
+
+        if running_process is not None:
+            if running_process.CPU_burst_time_1 == 0:
+                if running_process.IO_burst_time == 0 and running_process.CPU_burst_time_2 != 0:
+                    running_process.CPU_burst_time_2 -= 1
+                    running_elapsed_time += 1
+                    algorithm_procedure_output_file.write(
+                        "\tProcess-{}'s Second CPU Burst Was Executed for 1 Second (Remaining Second CPU Burst Time = {}).\n".format(
+                            running_process.process_id, running_process.CPU_burst_time_2))
+
+                    if running_process.CPU_burst_time_2 == 0:
+                        enqueue(terminated_queue, running_process)
+                        running_process.termination_time = current_time + 1
+                        running_process.turn_around_time = running_process.termination_time - running_process.arrival_time
+                        running_process.waiting_time = running_process.turn_around_time - (
+                                    running_process.CPU_burst_time_1 + running_process.IO_burst_time + running_process.CPU_burst_time_2)
+                        running_process = None
+                        algorithm_procedure_output_file.write(
+                            "\tProcess-{} Was Terminated (Moved From Running-State to Terminated-Queue).\n".format(
+                                rear(terminated_queue).process_id))
+                    elif running_process_previous_level == 1 and running_elapsed_time == time_quantum_level_1:
+                        enqueue(ready_queue_level_2, running_process)
+                        running_process = None
+                        algorithm_procedure_output_file.write(
+                            "\tProcess-{} Was Preempted (Moved From Running-State to Ready-Queue Level 2).\n".format(
+                                rear(ready_queue_level_2).process_id))
+                    elif running_process_previous_level == 2 and running_elapsed_time == time_quantum_level_2:
+                        enqueue(ready_queue_level_3, running_process)
+                        running_elapsed_time = 0
+                        algorithm_procedure_output_file.write(
+                            "\tProcess-{} Was Preempted (Moved From Running-State to Ready-Queue Level 3).\n".format(
+                                rear(ready_queue_level_3).process_id))
+            else:
+                running_process.CPU_burst_time_1 -= 1
+                running_elapsed_time += 1
+                algorithm_procedure_output_file.write(
+                    "\tProcess-{}'s First CPU Burst Was Executed for 1 Second (Remaining First CPU Burst Time = {}).\n".format(
+                        running_process.process_id, running_process.CPU_burst_time_1))
+
+                if running_process.CPU_burst_time_1 == 0 and running_process.IO_burst_time != 0:
+                    enqueue(waiting_queue, running_process)
+                    running_process = None
+                    algorithm_procedure_output_file.write(
+                        "\tProcess-{} Moved From Running-State to Waiting-Queue to Execute Its IO Burst.\n".format(
+                            rear(waiting_queue).process_id))
+                elif running_process_previous_level == 1 and running_elapsed_time == time_quantum_level_1:
+                    enqueue(ready_queue_level_2, running_process)
+                    running_process = None
+                    algorithm_procedure_output_file.write(
+                        "\tProcess-{} Was Preempted (Moved From Running-State to Ready-Queue Level 2).\n".format(
+                            rear(ready_queue_level_2).process_id))
+                elif running_process_previous_level == 2 and running_elapsed_time == time_quantum_level_2:
+                    enqueue(ready_queue_level_3, running_process)
+                    running_process = None
+                    algorithm_procedure_output_file.write(
+                        "\tProcess-{} Was Preempted (Moved From Running-State to Ready-Queue Level 3).\n".format(
+                            rear(ready_queue_level_3).process_id))
+
+        current_time += 1
 
     algorithm_procedure_output_file.close()
-
     analyze_algorithm(job_queue_copy, terminated_queue, algorithm_analysis_output_file)
     analyze_processes(job_queue_copy, terminated_queue, processes_analysis_output_file)
+    algorithm_analysis_output_file.close()
+    processes_analysis_output_file.close()
 
+
+def HRRN_scheduling_algorithm(job_queue):
+    create_directory("./output/HRRN")
+    algorithm_procedure_output_file = create_file("./output/HRRN/", "HRRN - Algorithm Procedure", ".log")
+    algorithm_analysis_output_file = create_file("./output/HRRN/", "HRRN - Algorithm Analysis", ".log")
+    processes_analysis_output_file = create_file("./output/HRRN/", "HRRN - Processes Analysis", ".log")
+
+    job_queue_copy = copy_queue(job_queue)
+    ready_queue = Queue(job_queue.capacity)
+    running_process = None
+    waiting_queue = Queue(job_queue.capacity)
+    terminated_queue = Queue(job_queue.capacity)
+
+    #
+    #
+    #
+
+    algorithm_procedure_output_file.close()
+    analyze_algorithm(job_queue_copy, terminated_queue, algorithm_analysis_output_file)
+    analyze_processes(job_queue_copy, terminated_queue, processes_analysis_output_file)
     algorithm_analysis_output_file.close()
     processes_analysis_output_file.close()
